ba543747c294a9cc4df2b0da1c95f190c7a4d299
TheCoderDude001 <joeygujral@gmail.com>
2020-05-19 23:30:54 +0530

    New project edits but with a doubt

--- baseClass.js
+++ baseClass.js
@@ -1,33 +1,30 @@
-class baseClass {
+class rainDrops {
 
-    constructor(x,y,h){
+    constructor(){
 
-        var options = {
-            'restitution':0.8,
-            'friction':1.0,
-            'density':1.0
+        var charstring = "1234567890abcde/#@!&"
+        this.x = Math.round(random(0,1200))
+        this.y = Math.round(random(-10,0))
+
+
+        this.yspeed = Math.round(random(0,50))
+        var number = Math.round(random(0,19))
+
+        this.displaychar = charstring.charAt(number);
+
+    }
+
+
+    fall(){
+
+
+        this.y = this.yspeed + this.y
+
+        if(this.y > height){
+
+
+            this.y = Math.round(random(0,-100))
+
         }
-
-        this.body = Bodies.polygon(x, y, 3, h, options);
-        this.width = width;
-        this.height = height;
-        World.add(world, this.body);
-
-
     }
-
-    display(){
-
-        var angle = this.body.angle;
-        push();
-        translate(this.body.position.x, this.body.position.y);
-        rotate(angle);
-        /*imageMode(CENTER);
-        image(this.image, 0, 0, this.width, this.height);*/
-        pop();
-
-    }
-
-
-
 }
--- sketch.js
+++ sketch.js
@@ -1,25 +1,18 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
-
 var raindrops = []
 var raindrop,x,y;
 
 function setup() {
   createCanvas(800,400);
 
-  engine = Engine.create();
-  world = engine.world;
 
-//for(i = 0; i < i+1; i++){
-  x = Math.random(5,795);
-  y = 0
 
-  raindrop = new baseClass(x,y,10);
+for(i = 0; i < 500; i++){
+  
 
-  raindrops.push(raindrop)
-//}
+  raindrop = new rainDrops
+
+  raindrops.slice(i);
+}
 
 
 
@@ -31,19 +24,8 @@ function setup() {
 function draw() {
   background(255,255,255);  
 
-  Engine.update(engine);
-
-  //for(var i=0; i<raindrops.length; i++){
-
-    x = Math.random(5,795);
-    y = 0
   
-    raindrop = new baseClass(x,y,10);
-  
-    raindrops.push(raindrop)
-
-
- // }
+  raindrop.fall();
 
 
 
